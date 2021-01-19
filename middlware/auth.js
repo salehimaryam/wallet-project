83e30b07250f285e82e7a5030bce4acece8e1f2c
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-19 17:29:30 +0330

    auth middlware

--- middlware/auth.js
+++ middlware/auth.js
@@ -7,13 +7,13 @@ exports.verifyUserToken = (req, res, next) => {
 
     try {
         
-        token = token.split(' ')[1] // Remove Bearer from string
+        token = token.split(' ')[1]
         
         if (token === 'null' || !token) return res.status(401).send('Unauthorized request');
-        let verifiedUser = jwt.verify(token,"jhhjhjhlhijdhlcjdjkchdscdkc");  // config.TOKEN_SECRET => 'secretKey'
+        let verifiedUser = jwt.verify(token,"jhhjhjhlhijdhlcjdjkchdscdkc");  
         if (!verifiedUser) return res.status(401).send('Unauthorized request')
 
-        req.user = verifiedUser; // user_id & user_type_id
+        req.user = verifiedUser; 
         next();
 
     } catch (error) {
@@ -27,15 +27,15 @@ exports.verifyAdminToken = async(req, res, next) => {
 
     try {
         
-        token = token.split(' ')[1] // Remove Bearer from string
+        token = token.split(' ')[1] 
         
         if (token === 'null' || !token) return res.status(401).send('Unauthorized request');
-        let verifiedUser = jwt.verify(token,"jhhjhjhlhijdhlcjdjkchdscdkc");  // config.TOKEN_SECRET => 'secretKey'
+        let verifiedUser = jwt.verify(token,"jhhjhjhlhijdhlcjdjkchdscdkc"); 
         verifiedUser = await users.findById(verifiedUser.id);
         if (!verifiedUser) return res.status(401).send('Unauthorized request')
         if (verifiedUser.role !== 'admin') return res.status(401).send('You are not admin user!')
 
-        req.user = verifiedUser; // user_id & user_type_id
+        req.user = verifiedUser; 
         next();
 
     } catch (error) {
